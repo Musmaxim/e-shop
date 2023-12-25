47b5065ad6b7c28aa159fb79904b758af345227c
Mus Maxim <musmaxim.musienko@yandex.ru>
2023-12-25 20:57:25 +0300

    :Добавлена болванка сайта

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import Container from "../Container";
+import FooterList from "./FooterList";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3></h3>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
