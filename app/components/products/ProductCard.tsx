b63717ca3e93318e383e19864aba3bc4c3c772e3
Mus Maxim <musmaxim.musienko@yandex.ru>
2024-01-12 17:38:24 +0300

    Пилим отдельную карточку товара

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -4,18 +4,25 @@ import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: any;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
 
- const productRating = data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / data.reviews.length;
+  const productRating =
+    data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+    data.reviews.length;
 
   return (
-    <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
-      <div className="flex flex-col items-center w-full gap-1">
+    <div
+      onClick={() => router.push(`/product/${data.id}`)}
+      className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm"
+    >
+      <div className="flex flex-col items-center w-full gap-1 p-3">
         <div className="aspect-square overflow-hidden relative w-full">
           <Image
             fill
@@ -26,7 +33,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         </div>
         <div className="mt-4">{truncateText(data.name)}</div>
         <div>
-          <Rating value={productRating} readOnly/>
+          <Rating value={productRating} readOnly />
         </div>
         <div>{data.reviews.length} reviews</div>
         <div className="font-semibold">{formatPrice(data.price)}</div>
